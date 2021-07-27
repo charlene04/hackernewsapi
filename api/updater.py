c7660a7cd9031bde2baaba881561bb805899005e
charlene04 <charlesugbana04@gmail.com>
2021-07-27 21:19:03 +0100

    phase

--- api/updater.py
+++ api/updater.py
@@ -3,5 +3,5 @@ from .job import *
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(syncdbtopstories, 'interval', minutes=1)
+    scheduler.add_job(syncdbtopstories, 'interval', minutes=5)
     scheduler.start()
